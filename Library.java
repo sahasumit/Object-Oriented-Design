db5c87d57903940df3ed3ce0008008f7898dcacf
sahasumit <sahasumit288@gmail.com>
2021-11-15 23:36:12 +0100

    added enum and constants

--- Library.java
+++ Library.java
@@ -1,4 +1,61 @@
-class Library {
+
+//Enum and constatnts
+ enum BookFormat{
+  HARDCOVER,
+  PAPERBACK,
+  AUDIO_BOOK,
+  EBOOK,
+  NEWSPAPER,
+  MAGAZINE,
+  JOURNAL,
+}
+
+enum BookStatus{
+  AVAILABLE,
+  RESERVED,
+  LOANED,
+  LOST,
+}
+
+enum ReservationStatus{
+  WAITING,
+  PENDING,
+  CANCELED,
+  NONE,
+}
+
+enum AccountStatus {
+  ACTIVE,
+  CLOSED,
+  CANCELED,
+  BLACKLISTED,
+  NONE,
+}
+
+class Address{
+  private String streetAddress;
+  private String city;
+  private String state;
+  private String zipCode;
+  private String country;
+}
+
+class Person{
+  private String name;
+  private Address address;
+  private String email;
+  private String phone;
+}
+
+class Constatnts{
+  public static final int MAX_BOOKS_ISSUED_TO_A_USER = 5;
+  public static final int MAX_LENDING_DAYS = 10;
+}
+
+
+
+
+public class Library {
   public static void main(String args[]) {
     System.out.println("Library management system");
   }
