bad43c4cea861cb1dfc9f7289e0507eea77f0535
sahasumit <sahasumit288@gmail.com>
2021-11-24 22:58:16 +0100

    initialize constant types

---
+++ ParkingLot.java
@@ -0,0 +1,53 @@
+//types and Constants
+enum VehicleType {
+  CAR,
+  TRUCK,
+  ELECTRIC,
+  VAN,
+  MOTORBIKE,
+}
+
+enum ParkingSpotType {
+  HANDICAPPED,
+  COMPACT,
+  LARGE,
+  MOTORBIKE,
+  ELECTRIC,
+}
+
+enum AccountStatus {
+  ACTIVE,
+  BLOCKED,
+  BANNED,
+  COMPROMISED,
+  ARCHIVED,
+  UNKNOWN,
+}
+
+enum ParkingTicketStatus {
+  ACTIVE,
+  PAID,
+  LOST,
+}
+
+class Address {
+  private String streetAddress;
+  private  String city;
+  private String state;
+  private String zipCode;
+  private String country;
+}
+
+class Person {
+  private String name;
+  private Address address;
+  private String email;
+  private String phone;
+}
+
+
+class ParkingLot{
+  public static void main(String args[]) {
+    System.out.println("Design a parking lot");
+  }
+}
