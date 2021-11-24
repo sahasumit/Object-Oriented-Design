e3f9a09a0c05f5b160d0cc651677d68c6b6624ec
sahasumit <sahasumit288@gmail.com>
2021-11-25 00:41:29 +0100

    add account type

--- ParkingLot.java
+++ ParkingLot.java
@@ -45,6 +45,35 @@ class Person {
   private String phone;
 }
 
+//Basically two types of account in our system Admin and Parking Attendant
+
+abstract class Account {
+  //Most of the method have public getter and setter function
+  private String userName;
+  private String password;
+  private Person person;
+  private AccountStatus status;
+
+  public boolean resetPassword(){
+    //Here will be the mechanism
+    //To avoid compilation errors returning true
+    return true;
+  }
+}
+
+class Admin extends Account{
+  public bool addParkingFloor(ParkingFloor floor);
+  public bool addParkingSpot(string floorName, ParkingSpot spot);
+  public bool addParkingDisplayBoard(String floorName, ParkingDisplayBoard displayBoard);
+  public bool addCustomerInfoPanel(String floorName, CustomerInfoPanel infopanel);
+
+  public bool addEntrancePanel(EntrancePanel entrancePanel);
+  public bool addExitPanel(ExitPanel exitPanel);
+}
+
+class ParkingAttendant extends Account {
+  public bool processTicket(String ticketNumber);
+}
 
 class ParkingLot{
   public static void main(String args[]) {
